a2f4a9bc5e4761d37402ee39f39fcf65553f2294
Csaba Kiraly <csaba.kiraly@gmail.com>
2024-10-29 22:25:20 +0100

    add minimal run script with bootnode and results folder
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BOOTNODE="enr:-Ku4QHqVeJ8PPICcWk1vSn_XcSkjOkNiTg6Fmii5j6vUQgvzMc9L1goFnLKgXqBJspJjIsB91LTOleFmyWWrFVATGngBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpC1MD8qAAAAAP__________gmlkgnY0gmlwhAMRHkWJc2VjcDI1NmsxoQKLVXFOhp2uX6jeT0DvvDpPcU8FWMjQdR4wMuORMhpX24N1ZHCCIyg"
+
+WORKDIR="results/`date -Iseconds`"
+echo $WORKDIR
+mkdir -p "$WORKDIR"
+cd "$WORKDIR"
+
+../../build/dcrawl --bootnode="$BOOTNODE" "$@"

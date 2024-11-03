4d66135396834d4a565d193265606e6bdb49e2d3
Csaba Kiraly <csaba.kiraly@gmail.com>
2024-11-03 17:19:49 +0100

    plot pie chart based on forkDigest
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- postprocessing/plot.py
+++ postprocessing/plot.py
@@ -12,3 +12,7 @@ plt.savefig('rtt-cdf.png')
 measurements[['bwMaxMbps', 'bwAvgMbps']].plot.hist(
     cumulative=True, density=True, histtype='step', bins=10000, figsize=(10, 5))
 plt.savefig('bw-cdf.png')
+
+measurements[['node_id', 'forkDigest']].groupby('forkDigest').nunique().sort_values(
+    by='node_id',ascending=False).plot(kind='pie', y='node_id', autopct='%1.0f%%', legend=False)
+plt.savefig('forkDigest.png')

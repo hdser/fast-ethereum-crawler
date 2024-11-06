1f8b50abb73f191823764b29448f6aa6fc528fe7
Csaba Kiraly <csaba.kiraly@gmail.com>
2024-11-06 16:35:48 +0100

    improve forkDigest plot
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- postprocessing/plot.py
+++ postprocessing/plot.py
@@ -1,7 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 
-measurements = pandas.read_csv("peerstore.csv")
+measurements = pandas.read_csv("peerstore.csv", quotechar="'")
 print(measurements.describe)
 print(measurements.nunique())
 
@@ -13,6 +13,35 @@ measurements[['bwMaxMbps', 'bwAvgMbps']].plot.hist(
     cumulative=True, density=True, histtype='step', bins=10000, figsize=(10, 5))
 plt.savefig('bw-cdf.png')
 
-measurements[['node_id', 'forkDigest']].groupby('forkDigest').nunique().sort_values(
-    by='node_id',ascending=False).plot(kind='pie', y='node_id', autopct='%1.0f%%', legend=False)
+def networkMapper(row):
+    enr = row[' enr']
+    if " eth2:" in enr:
+        return "Eth consensus node on " + row['forkDigest']
+    elif " eth:" in enr:
+        return "Eth execution node"
+    elif " opstack:" in enr:
+        return "Optimism execution node"
+    elif " ssv:" in enr:
+        return "SSV ?"
+    elif " opera:" in enr:
+        return "Opera ?"
+    elif " opel:" in enr:
+        return "Opel ?"
+    elif " c:" in enr:
+        return "C ?"
+    else:
+        #print(enr)
+        return "Unknown"
+
+print(measurements['forkDigest'].value_counts())
+measurements['network'] = measurements.apply(networkMapper, axis=1)
+
+nodeCnt = len(measurements['network'])
+def myFmt(x):
+    return '{:.0f}%\n{:.0f}'.format(x, nodeCnt*x/100)
+measurements[['node_id', 'network']].groupby('network').nunique().sort_values(
+    by='node_id',ascending=False).plot(
+        kind='pie', y='node_id', autopct=myFmt,
+        legend=False, figsize=(10, 6), ylabel='',
+    )
 plt.savefig('forkDigest.png')
